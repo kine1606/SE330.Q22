96d48d609fdc80a9858ea893cbecbd1b710e60a5
kine1606 <137513694+kine1606@users.noreply.github.com>
2026-05-28 12:40:27 +0700

    add docker config file

---
+++ product-service/src/test/java/com/SE330_Q22/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.SE330_Q22.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
