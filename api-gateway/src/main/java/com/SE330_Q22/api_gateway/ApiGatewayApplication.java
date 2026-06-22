1b694e4c1bf8b661642b030948a1fea34d7be03c
Horpes66 <sonhothienbao@gmail.com>
2026-06-22 12:33:30 +0700

    fix: set JVM timezone to Asia/Ho_Chi_Minh in all main classes

--- api-gateway/src/main/java/com/SE330_Q22/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/SE330_Q22/api_gateway/ApiGatewayApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiGatewayApplication {
     public static void main(String[] args) {
+        java.util.TimeZone.setDefault(java.util.TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 }
--- order-service/src/main/java/com/SE330_Q22/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/SE330_Q22/order_service/OrderServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
+		java.util.TimeZone.setDefault(java.util.TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
